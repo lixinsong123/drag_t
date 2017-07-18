af8f0002536661f7e5fdb41b5c3911d9505cc7d3
lixinsong123 <18875044805.@163com>
2017-07-18 09:32:27 +0800

    success drag

--- drag.js
+++ drag.js
@@ -1,9 +1,16 @@
-window.onload=function(){
+
 	function drag(obj){
 		obj.onmousedown=function(ev){
 			var ev = ev ||window.event;
 			var disX = ev.clientX - obj.offsetLeft;
 			var disY = ev.clientY - obj.offsetTop;
+			document.onmousemove=function(ev){
+					var ev = ev||window.event;
+					obj.style.left=ev.clientX-disX+"px";
+					obj.style.top =ev.clientY-disY+"px";
+			}
+			document.onmouseup=function(){
+				document.onmousemove=document.onmouseup=null;
+			}
 		}
 	}
-}
